dfa5bcce93c4051261f77e9a27421e4223e3c1a0
Andrew Kiellor <akiellor@@users.noreply.github.com>
2016-04-06 23:19:03 -0700

    Add osx stuff.

---
+++ manifests/osx.pp
@@ -0,0 +1,29 @@
+class akiellor::osx {
+  class { 'osx::dock::position':
+    position => 'right'
+  }
+
+  class { 'osx::global::key_repeat_rate':
+    rate => 0
+  }
+
+  class { 'osx::global::key_repeat_delay':
+    delay => 0
+  }
+
+  include osx::keyboard::capslock_to_control
+
+  boxen::osx_defaults { 'Make function keys do real things, and not apple things':
+    user   => $::boxen_user,
+    key    => 'com.apple.keyboard.fnState',
+    domain => 'NSGlobalDomain',
+    value  => 'true',
+  }
+
+  boxen::osx_defaults { 'Make tab key do real things, and not apple things':
+    user   => $::boxen_user,
+    key => 'AppleKeyboardUIMode',
+    domain => 'NSGlobalDomain',
+    value => '2',
+  }
+}

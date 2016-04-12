b264ad31330074a07f5c974dc7d4e5908b8f9cdc
Andrew Kiellor <akiellor@@users.noreply.github.com>
2016-04-12 00:16:31 -0700

    Add docker tld and nameserver.

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -1,6 +1,11 @@
 class akiellor::dns {
   include dnsmasq
 
+  file { '/etc/resolver/docker':
+    ensure => present,
+    content => "nameserver 127.0.0.1"
+  }
+
   boxen::env_script { 're-dns':
     content => "alias re-dns='echo \"address=/docker/$(docker-machine ip default)\" | sudo tee ${dnsmasq::searchdir}/docker.conf && sudo launchctl stop dev.dnsmasq && sudo launchctl start dev.dnsmasq\'"
   }

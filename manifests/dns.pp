92682bfed5ac352c2ef88aeeae56e088d1bb6ba6
Andrew Kiellor <akiellor@@users.noreply.github.com>
2016-04-20 20:23:46 -0700

    Remove docker dns stuff.

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -4,21 +4,12 @@ class akiellor::dns {
   file { ['/etc/resolver/docker',
           '/etc/resolver/local',
           "${dnsmasq::searchdir}/docker.conf",
-          '/Library/LaunchDaemons/dev.docker-dns.plist']:
+          '/Library/LaunchDaemons/dev.docker-dns.plist',
+          '/Library/LaunchDaemons/dev.dockerdns.plist']:
     ensure => absent
   }
 
-
-  file { '/Library/LaunchDaemons/dev.dockerdns.plist':
-    ensure => present,
-    owner  => "root",
-    source => "puppet:///modules/akiellor/dev.docker-dns.plist",
-  } -> service { 'dev.dockerdns':
-    ensure => running,
-    enable => true
-  }
-
   boxen::env_script { 're-dns':
-    content => "alias re-dns='sudo launchctl stop dev.dockerdns && sudo launchctl start dev.dockerdns\'"
+    content => "# empty"
   }
 }
